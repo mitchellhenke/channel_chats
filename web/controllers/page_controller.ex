f3420e65379e876018f0fdb073ec19651b179772
Mitchell Henke <mitchh23@gmail.com>
2015-07-05 13:58:25 -0500

    pass messages logged to view

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule ChannelChats.PageController do
   use ChannelChats.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    messages = ChatLog.get_logs("rooms:lobby")
+    |> parse_logs
+    render conn, "index.html", messages: messages
   end
+
+  def parse_logs([]), do: []
+  def parse_logs([{_room, messages}]), do: Enum.reverse(messages)
 end
