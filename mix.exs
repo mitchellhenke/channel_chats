454c0c62f1d6e4016dcd17e3984409d2941962fa
Mitchell Henke <mitchh23@gmail.com>
2015-08-02 11:03:33 -0500

    update version of app

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ChannelChats.Mixfile do
 
   def project do
     [app: :channel_chats,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
