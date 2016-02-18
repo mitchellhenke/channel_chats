294c8aa3a40134757d4003d4448fdc4345192219
Mitchell Henke <mitchh23@gmail.com>
2016-02-18 09:45:05 -0600

    update to phoenix 1.1

--- mix.exs
+++ mix.exs
@@ -28,8 +28,8 @@ defmodule ChannelChats.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.0.0"},
-     {:phoenix_html, "~> 2.0"},
+    [{:phoenix, "~> 1.1.0"},
+     {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
      {:exrm, "~> 0.18"}
